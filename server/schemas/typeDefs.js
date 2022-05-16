748e2bed0ea0a53b574ce1c78740522f4cf32690
Angela Donati <angeladonati93@gmail.com>
2022-05-16 10:12:57 -0400

    create typeDefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,1 +1,48 @@
-// 
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+  type Book {
+    _id: ID!
+    title: String!
+    authors: [String]
+    description: String
+    bookId: String!
+    image: String
+    link: String
+  }
+
+  type User {
+    _id: ID!
+    username: String!
+    email: String!
+    savedBooks: [Book]
+    bookCount: Int
+  }
+
+  type Auth {
+      token: ID!
+      user: user
+  }
+
+  input bookInput {
+      bookId: String
+      authors: [String]
+      description: String
+      image: String
+      link: String
+      title: String
+
+  }
+  type Query {
+    me: user
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(input: bookInput!): User
+    removeBook(bookId: ID!): User
+  }
+`;
+
+module.exports = typeDefs;
